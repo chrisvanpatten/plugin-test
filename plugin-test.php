7a168154b024a63c7345fb3445f665ffce35c931
Chris Van Patten <chris@vanpattenmedia.com>
2019-02-08 15:31:18 -0500

    💥

---
+++ plugin-test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: My Test Plugin
+ * Plugin URI: https://github.com/chrisvanpatten/my-test
+ * Description: This is my test plugin
+ * Version: 1.0.0
+ * Author: Chris Van Patten
+ * License: CC0
+ */
+
+echo 'This is my initial commit.';
